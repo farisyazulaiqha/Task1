826b43333a79e11e74c01be9faa81932a305ee2b
farisyazulaiqha <48311363+farisyazulaiqha@users.noreply.github.com>
2019-04-23 16:45:33 +0800

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,27 @@
+import java.io.*;
+	import java.net.*;
+	
+	public class server{
+		
+		 public static void main(String[] args) throws 
+	IOException{
+		
+		ServerSocket serverSocket =new ServerSocket(10000);
+		Socket clientSocket = serverSocket.accept();
+	
+		System.out.println("client connected");
+	  
+		InputStreamReader gt =new 
+	InputStreamReader(clientSocket.getInputStream());
+	
+		BufferedReader ot = new BufferedReader (gt);
+	
+		PrintWriter rp = new 
+	PrintWriter(clientSocket.getOutputStream());
+		rp.println("wo hen hao");
+		rp.flush();
+	
+		String recvr = ot.readLine();
+		System.out.println(recvr);
+		}
+	}
